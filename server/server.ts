79ea96f54134e40dc765a9cf858aa12db44dd035
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-07-31 13:09:18 +0530

    add: mongoDB and Redis connections

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+import { app } from "./app";
+import connectDB from "./utils/database";
+require('dotenv').config()
+
+
+// create server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log("=================================")
+    console.log(`Server started on PORT ${PORT}`)
+    connectDB()
+})
