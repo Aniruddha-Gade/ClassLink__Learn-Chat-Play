2f86114e9b07a5f091909caa354cc3963a130e33
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-05 18:11:30 +0530

    add: edit-course controller and route

--- server/controller/course.controller.ts
+++ server/controller/course.controller.ts
@@ -4,6 +4,7 @@ import { catchAsyncError } from '../utils/catchAsyncError';
 import { NextFunction, Request, Response } from 'express';
 import ErrorHandler from '../utils/ErrorHandler';
 import { createCourse } from '../services/course.service';
+import CourseModel from '../models/course.model';
 
 
 // =========================== UPLOAD COURSE ===========================
@@ -28,6 +29,57 @@ export const uploadCourse = catchAsyncError(async (req: Request, res: Response, 
 
         // create course in DB
         createCourse(data, res, next)
+
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400, "Error while creating course"));
+    }
+})
+
+
+
+
+// =========================== UPLOAD COURSE ===========================
+export const editCourse = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        // extract data 
+        const data = req.body;
+        const thumbnail = data.thumbnail
+
+
+        // delete previous thumbnail and upload new one
+        if (thumbnail) {
+            await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+            const uploadedUrl = await cloudinary.v2.uploader.upload(thumbnail, {
+                folder: "ClassLink/courses",
+            })
+
+            // store urls 
+            data.thumbnail = {
+                public_id: uploadedUrl.public_id,
+                url: uploadedUrl.secure_url,
+            }
+        }
+
+        // update course in DB
+        const courseId = req.params.id
+        const updatedCourse = await CourseModel.findByIdAndUpdate(courseId,
+            {
+                $set: data,
+            },
+            { new: true }
+        )
+
+        if (!updatedCourse) {
+            return next(new ErrorHandler("Course could not found to edit", 400, "Error while creating course"));
+        }
+
+        // return response
+        res.status(201).json({
+            success: true,
+            updatedCourse,
+            message: "Course updated successfully"
+        })
 
     } catch (error) {
         return next(new ErrorHandler(error.message, 400, "Error while creating course"));
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { isAuthenticated, isInstructor } from "../middleware/auth"
-import { uploadCourse } from "../controller/course.controller"
+import { editCourse, uploadCourse } from "../controller/course.controller"
 
 const courseRouter = Router()
 
 
 courseRouter.post("/create-course", isAuthenticated, isInstructor, uploadCourse)
+courseRouter.put("/edit-course/:id", isAuthenticated, isInstructor, editCourse)
 
 
 export default courseRouter
