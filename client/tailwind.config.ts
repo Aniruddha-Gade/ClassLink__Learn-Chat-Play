8732777fe76683ea37aa6f939d88de641560e518
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-09-10 12:05:48 +0530

    fix error related to tailwind.config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -34,29 +34,29 @@ const config: Config = {
         '1500px': '1500px',
         '800px': '800px',
         '400px': '400px',
+      },
 
 
-
-        keyframes: {
-          "accordion-down": {
-            from: { height: "0" },
-            to: { height: "var(--radix-accordion-content-height)" },
-          },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
-            to: { height: "0" },
-          },
-          "caret-blink": {
-            "0%,70%,100%": { opacity: "1" },
-            "20%,50%": { opacity: "0" },
-          },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
-          "caret-blink": "caret-blink 1.25s ease-out infinite",
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
         },
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
+
     },
     plugins: [require("tailwindcss-animate")],
   }
