e91258340f384e338299f1257aa78c20028424a9
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-09-10 11:58:09 +0530

    change lorem data in home page

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -8,49 +8,79 @@ const Hero = () => {
         <div className="bg-white dark:bg-black text-black dark:text-white h-full flex flex-col gap-5 ">
 
 
-            <div className='flex flex-col gap-5 '>
-                <p>
-                    Aniruddha gade Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <div className='flex flex-col gap-10 p-10 '>
+                <h1 className='text-5xl text-green-600 font-bold '>
+                    ClassLink - Learn , Chat, Play
+                </h1>
+               <p>
+               Nature is the connection between the physical world surrounding us and the life inside us. 
+               Nature is Gods most precious and valuable gift to humans. It is the principal source of all essential 
+               nutrients for all living things on the planetis one of the topics on which we might be 
+               asked to write a paragraph. Check the samples provided in the article to learn how to write one on your own.
+               </p>
 
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-                <p>
-                    Where does it come from?
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                </p>
+               <p>
+               Everything we see around us constitutes nature, including the sun, the moon, trees, flowers, fruits,
+                human beings, birds, animals, etc. In nature, everyone depends on one another to keep the ecosystem 
+                healthy. For survival, every creature is interrelated and reliant on one another. Humans, 
+                for example, rely on nature for their survival, and nature provides us with oxygen, food, water, 
+                shelter, medicines, and clothing, among other things. Many shades may be seen in nature, 
+                contributing to the planets beauty. Along with humans, animals and birds also find their habitat 
+                and means of survival in nature. Therefore, it is essential to take proper care of our nature to 
+                maintain a healthy life.
+               </p>
 
-                <p>
-                    Why do we use it?
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                </p>
-                <p>
-                    Aniruddha gade Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+               <p>
+               Nature is beautiful, yet it is difficult to put into words. Nature is honoured with a variety of
+                religious traditions. The primary source of life on Earth is the components that exist naturally. 
+                All of the elements are linked. Natural ingredients can never be substituted. Humans process 
+                natural materials for use in todays ever-changing world and destroy their rawness and 
+                individuality. Human activities have an impact on nature, and as a result, the quality of the 
+                environment is deteriorating. Deterioration of nature is primarily caused by pollution of the 
+                air and water. The oxygen produced by a single fully-grown tree is enough to supply ten people, 
+                and the amount of oxygen released by a forest is unquestionably adequate for a metropolis or town.
+                 Nature is a healer, and it is the foundation for many industries. Nature, on the other hand, 
+                 is both a giver and a taker, according to legends. Earthquakes, volcanic eruptions, floods, and
+                  cyclones are examples of natural calamities. These disasters, which occur as a result of natural
+                   causes, have resulted in significant damage. The benefits of sunlight and fresh air to our
+                    health cannot be overstated. Nature has a variety of beneficial effects on our bodies. For 
+                    recuperation and vacations, people choose natural settings. People prefer to raise their 
+                    children in the countryside in the United States. Their primary goal is to introduce their 
+                    children to the benefits of natural beauty.
+               </p>
 
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-                <p>
-                    Where does it come from?
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                </p>
+               <p>
+               Everything we see around us constitutes nature, including the sun, the moon, trees, flowers, fruits,
+                human beings, birds, animals, etc. In nature, everyone depends on one another to keep the ecosystem 
+                healthy. For survival, every creature is interrelated and reliant on one another. Humans, 
+                for example, rely on nature for their survival, and nature provides us with oxygen, food, water, 
+                shelter, medicines, and clothing, among other things. Many shades may be seen in nature, 
+                contributing to the planets beauty. Along with humans, animals and birds also find their habitat 
+                and means of survival in nature. Therefore, it is essential to take proper care of our nature to 
+                maintain a healthy life.
+               </p>
 
-                <p>
-                    Why do we use it?
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                </p>
-                <p>
-                    Aniruddha gade Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+               <p>
+               Everything we see around us constitutes nature, including the sun, the moon, trees, flowers, fruits,
+                human beings, birds, animals, etc. In nature, everyone depends on one another to keep the ecosystem 
+                healthy. For survival, every creature is interrelated and reliant on one another. Humans, 
+                for example, rely on nature for their survival, and nature provides us with oxygen, food, water, 
+                shelter, medicines, and clothing, among other things. Many shades may be seen in nature, 
+                contributing to the planets beauty. Along with humans, animals and birds also find their habitat 
+                and means of survival in nature. Therefore, it is essential to take proper care of our nature to 
+                maintain a healthy life.
+               </p>
 
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-                <p>
-                    Where does it come from?
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                </p>
-
-                <p>
-                    Why do we use it?
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                </p>
+               <p>
+               Everything we see around us constitutes nature, including the sun, the moon, trees, flowers, fruits,
+                human beings, birds, animals, etc. In nature, everyone depends on one another to keep the ecosystem 
+                healthy. For survival, every creature is interrelated and reliant on one another. Humans, 
+                for example, rely on nature for their survival, and nature provides us with oxygen, food, water, 
+                shelter, medicines, and clothing, among other things. Many shades may be seen in nature, 
+                contributing to the planets beauty. Along with humans, animals and birds also find their habitat 
+                and means of survival in nature. Therefore, it is essential to take proper care of our nature to 
+                maintain a healthy life.
+               </p>
             </div>
 
         </div>
