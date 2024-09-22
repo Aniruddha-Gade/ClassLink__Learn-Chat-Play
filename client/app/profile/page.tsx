8acdf52e9d5bf262b35e012aa7547e82aa50227f
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-09-22 13:46:06 +0530

    add: empty profile page

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const page: React.FC<Props> = () => {
+    return (
+        <div>
+            This is profile page
+        </div>
+    )
+}
+
+export default page
