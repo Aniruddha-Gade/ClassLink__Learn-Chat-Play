5bd85b849a6a09c83bdafc8b51b59b73de86ffcb
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-09-22 19:22:01 +0530

    add: custom hooks for frontend protected route

---
+++ client/app/hooks/useProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+import React from "react";
+import { useSelector } from "react-redux";
+
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+
+export default function useProtected({ children }: ProtectedProps) {
+  const { user, token } = useSelector((state: any) => state.auth);
+
+  const isAuthenticated = user && token ? true : false
+
+  return isAuthenticated ? children : redirect("/");
+}
