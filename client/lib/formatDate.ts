ae91055cda5f2c2435b0c12f64e1c97365956716
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-09-25 23:58:29 +0530

    add: formatDate function

---
+++ client/lib/formatDate.ts
@@ -0,0 +1,18 @@
+export function formatDate(isoString: string): string {
+    const date = new Date(isoString);
+
+    // Get parts of the date
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        // second: '2-digit', 
+        hour12: true
+    };
+
+    // Format the date
+    return date.toLocaleDateString('en-US', options);
+}
+
