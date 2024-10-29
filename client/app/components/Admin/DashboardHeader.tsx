ed45bdcd4f150aff2458452b72c363001f5f0ab7
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-10-29 13:50:09 +0530

    add: notification dropdown-menu for admin dashboard

--- client/app/components/Admin/DashboardHeader.tsx
+++ client/app/components/Admin/DashboardHeader.tsx
@@ -5,69 +5,86 @@ import React, { FC, useState } from "react";
 import Image from 'next/image';
 
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
+
+import { Button } from "../ui/button"
+
+
+
 type Props = {};
 
 const DashboardHeader: FC<Props> = () => {
-    const [open, setOpen] = useState(false);
+
 
     return (
         <div className="w-full flex items-center justify-end p-6 fixed top-3 right-0">
             <ThemeSwitcher />
 
 
-            <div
-                className="relative cursor-pointer m-2"
-                onClick={() => setOpen(!open)}
-            >
-                <Image
-                    src='/assets/icons/notification-icon.png'
-                    className="cursor-pointer object-contain w-7 h-7 "
-                    width={30}
-                    height={30}
-                    alt="Profile Icon"
-                />
 
-                <span className="absolute -top-2 -right-2 bg-[#3ccba0] rounded-full w-[20px] h-[20px] text-[12px] flex items-center justify-center text-white">
-                    3
-                </span>
-            </div>
+            <DropdownMenu >
+                <DropdownMenuTrigger>
+                    <div
+                        className="relative cursor-pointer m-2"
+                    >
+                        <Image
+                            src='/assets/icons/notification-icon.png'
+                            className="cursor-pointer object-contain w-7 h-7 "
+                            width={30}
+                            height={30}
+                            alt="Profile Icon"
+                        />
 
-
-            {open && (
-                <div
-                    className="w-[350px] h-[50vh] dark:bg-[#111c43] bg-white shadow-xl absolute top-16 z-10 rounded"
-                >
-                    <h5 className="text-center text-[20px] font-Poppins text-black dark:text-white p-3">
-                        Notifications
-                    </h5>
-                    <div className="dark:bg-[#2d3ae1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
-                        <div className="w-full flex items-center justify-between p-2">
-                            <p className="text-black dark:text-white">New Question Received</p>
-                            <p className="text-black dark:text-white cursor-pointer">Mark as read</p>
-                        </div>
-                        <p className="px-2 text-black dark:text-white">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, sequi! Tempore libero omnis et, ea beatae ut, itaque
-                        </p>
-                        <p className="p-2 text-black dark:text-white text-[14px]">
-                            5 days ago
-                        </p>
+                        <span className="absolute -top-2 -right-2 bg-[#3ccba0] rounded-full w-[20px] h-[20px] text-[12px] flex items-center justify-center text-white">
+                            3
+                        </span>
                     </div>
-                    <div className="dark:bg-[#2d3ae1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
-                        <div className="w-full flex items-center justify-between p-2">
-                            <p className="text-black dark:text-white">New Question Received</p>
-                            <p className="text-black dark:text-white cursor-pointer">Mark as read</p>
+                </DropdownMenuTrigger>
+
+
+                <DropdownMenuContent className='w-[300px] h-[500px] rounded-2xl border-2 duration-300 mr-7'>
+                    <DropdownMenuLabel className='text-center text-[20px] font-Poppins text-black dark:text-white p-3'>Notifications</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
+                        <div>
+                            <div className="w-full flex items-center justify-between ">
+                                <p className="text-black dark:text-white">New Question Received</p>
+                                <p className="cursor-pointer text-green-600">Mark as read</p>
+                            </div>
+                            <p className="text-black dark:text-white">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, sequi! Tempore libero omnis et, ea beatae ut, itaque
+                            </p>
+                            <p className=" text-black dark:text-white text-[14px]">
+                                5 days ago
+                            </p>
                         </div>
-                        <p className="px-2 text-black dark:text-white">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, sequi! Tempore libero omnis et, ea beatae ut, itaque
-                        </p>
-                        <p className="p-2 text-black dark:text-white text-[14px]">
-                            5 days ago
-                        </p>
-                    </div>
-                </div>
-            )}
+                    </DropdownMenuItem>
 
+                    <DropdownMenuItem className="font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
+                        <div>
+                            <div className="w-full flex items-center justify-between ">
+                                <p className="text-black dark:text-white">New Question Received</p>
+                                <p className="cursor-pointer text-green-600">Mark as read</p>
+                            </div>
+                            <p className="text-black dark:text-white">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, sequi! Tempore libero omnis et, ea beatae ut, itaque
+                            </p>
+                            <p className="text-black dark:text-white text-[14px]">
+                                5 days ago
+                            </p>
+                        </div>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
 
+            
         </div>
     );
 };
