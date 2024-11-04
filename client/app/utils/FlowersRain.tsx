ce79bb0c32cf8f8f00e2af94cc96b8c464eee39f
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-11-04 13:00:29 +0530

    remove 'react-use' package

--- client/app/utils/FlowersRain.tsx
+++ client/app/utils/FlowersRain.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
 interface Props {
