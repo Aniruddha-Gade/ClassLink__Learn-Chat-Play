e7f66131df69ce4508cc80579f3e9668b49029df
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-11-19 14:40:27 +0530

    cloud-loader component

---
+++ client/app/components/Loader/LoadingCloud.tsx
@@ -0,0 +1,13 @@
+
+
+ const LoadingCloud = () => {
+  return (
+    <div className="cloud-loader-overlay">
+        <div className="cloud-loader-container">
+          <div className="cloud-loader"></div>
+        </div>
+    </div>
+  );
+};
+
+export default LoadingCloud
